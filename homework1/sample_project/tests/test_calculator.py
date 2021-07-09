808d5cf52a41d229f34185286e06a0e2d4d0b5af
xemnes5 <razielxem@gmail.com>
2021-07-09 15:08:54 +0300

    switch branches

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -1,7 +1,6 @@
 import pytest
 
-# from homework1.sample_project.calculator.calc import check_power_of_2
-from calculator.calc import check_power_of_2
+from homework1.sample_project.calculator.calc import check_power_of_2
 
 
 def test_positive_case():
